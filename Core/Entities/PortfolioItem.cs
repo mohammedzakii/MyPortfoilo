dd47ed60baffb717b4e0c3e4fd2ab06c97927760
NV <NV@LAPTOP-GLR3PGDQ>
2022-02-17 00:29:01 +0200

    Add project files.

---
+++ Core/Entities/PortfolioItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Entities
+{
+    public class PortfolioItem : EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+    }
+}
+
+
